f62a3525382eb89128c734576b519f6476a7b0ae
mcwachira <23430963+mcwachira@users.noreply.github.com>
2022-07-21 09:55:33 +0300

    added sign in with google  popup method.

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { signInWithGooglePopup } from '../../utils/firebase/firebase.utils'
+
+const LoginGoogle = async () => {
+  const response =  await signInWithGooglePopup()
+  console.log(response)
+}
 
 const SignIn = () => {
   return (
     <div>
         <h1> sign in </h1>
+        <button onClick={LoginGoogle}> sign in with google</button>
     </div>
   )
 }
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -3,15 +3,39 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+import {
+    getAuth,
+    signInWithRedirect,
+    signInWithPopup,
+    GoogleAuthProvider //class
+    
+} from 'firebase/auth'
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_AUTH_DOMAIN,
-    projectId: process.env.REACT_PROJECT_ID,
-    storageBucket: process.env.REACT_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_SENDER_ID,
-    appId: process.env.REACT_APP_ID
-};
+    apiKey:'AIzaSyA3qxZiTJp14pERpa4rQgoZivHlZBs8QK8',
+    authDomain:'swag-v2-eccomerce.firebaseapp.com',
+    projectId:'swag-v2-eccomerce',
+    storageBucket:'swag-v2-eccomerce.appspot.com',
+    messagingSenderId: '1035209681336',
+    appId: '1:1035209681336:web:c7fa0d6a7f04ba7b38c74d'
+
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
+
+
+//creating a new instance of the google auth provider
+const provider = new GoogleAuthProvider()
+
+//this forces a user to select an account each time
+provider.setCustomParameters({
+    prompt: 'select_account'
+})
+
+export const auth =  getAuth();
+
+export const signInWithGooglePopup = () => signInWithPopup(auth , provider)
+
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, provider)
